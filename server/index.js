1904efdfa8e6f02000097db7e4d9cba54e4b7936
Nick <nickpereslugoff@gmail.com>
2019-03-15 21:57:32 -0400

    changed middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express')
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers')
@@ -11,13 +13,13 @@ const server = new ApolloServer({ typeDefs, resolvers });
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(express.static(path.join(__dirname, '../public/')))
-server.applyMiddleware({ app, path: '/graphql' });
+
 
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-const port = process.env.PORT || 3000;
+server.applyMiddleware({app});
 app.listen(port, function () {
   console.log("Knock, knock");
   console.log("Who's there?");
