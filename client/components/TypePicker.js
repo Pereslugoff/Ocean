cff3ef0f06f5dac2ac3b0e4d3c368c8c52b1ea8e
Nick <nickpereslugoff@gmail.com>
2019-04-21 15:25:14 -0400

    Added sass

--- client/components/TypePicker.js
+++ client/components/TypePicker.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const TypePicker = (props) => {
-  const {handleType} = props
+const TypePicker = props => {
+  const { handleType } = props;
   return (
     <div className="picker-container">
-      <button className="btn" onClick={() => handleType("subreddit")}>Search Subreddits</button>
-      <button className="btn"onClick={() => handleType("user")}>Search Users</button>
+      <div className="type-selection" onClick={() => handleType("subreddit")}>
+        Search Subreddits
+      </div>
+      <span className="type-border" />
+      <div className="type-selection" onClick={() => handleType("user")}>
+        Search Users
+      </div>
     </div>
   );
 };
--- public/bundle.js
+++ public/bundle.js
@@ -98,18 +98,91 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _PersonalityCalculator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./PersonalityCalculator */ "./client/components/PersonalityCalculator.js");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
-var App = function App() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "app-container"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    id: "app-header"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Ocean"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "A personality calculator for Reddit")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "calc-container"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_PersonalityCalculator__WEBPACK_IMPORTED_MODULE_1__["default"], null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_PersonalityCalculator__WEBPACK_IMPORTED_MODULE_1__["default"], null)));
-};
+
+var App =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(App, _Component);
+
+  function App() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, App);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(App)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      numCalculators: 1
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "addCalculator", function () {
+      var numCalculators = _this.state.numCalculators;
+
+      _this.setState({
+        numCalculators: numCalculators + 1
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: "render",
+    value: function render() {
+      var calculators = [];
+      var numCalculators = this.state.numCalculators;
+
+      for (var i = 0; i < numCalculators; i++) {
+        calculators.push(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_PersonalityCalculator__WEBPACK_IMPORTED_MODULE_1__["default"], null));
+      }
+
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "app-container"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "app-header"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Ocean"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "A personality calculator for Reddit")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "calc-container"
+      }, calculators, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "add-calculator",
+        onClick: this.addCalculator
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "line"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "line"
+      }))));
+    }
+  }]);
+
+  return App;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
 /* harmony default export */ __webpack_exports__["default"] = (App);
 
@@ -702,13 +775,15 @@ var TypePicker = function TypePicker(props) {
   var handleType = props.handleType;
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "picker-container"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    className: "btn",
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "type-selection",
     onClick: function onClick() {
       return handleType("subreddit");
     }
-  }, "Search Subreddits"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    className: "btn",
+  }, "Search Subreddits"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    className: "type-border"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "type-selection",
     onClick: function onClick() {
       return handleType("user");
     }
