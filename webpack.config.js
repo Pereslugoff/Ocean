d415cdb5205ed73a69327161ca1e5a5513db4871
Nick <nicholas.pereslugoff@aexp.com>
2020-10-23 18:34:39 -0400

    fix(webpack): updated config and added build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,59 @@
+const path = require('path');
+// module.exports = {
+//   entry: './client/index.jsx', // assumes your entry point is the index.js in the root of your project folder
+//   mode: 'development',
+//   output: {
+//     path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+//     filename: './public/bundle.js'
+//   },
+//   devtool: 'source-maps',
+//   module: {
+//     rules  : [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           query: {
+//             presets: ['es2015', 'react']
+//           }
+//         }
+//       },
+//       {
+// 				test: /\.css$/,
+// 				use: [{
+// 					loader: 'style-loader'
+// 					}, {
+// 						loader: 'css-loader',
+// 						options: {
+// 							modules: true,
+// 							localIdentName: '[name]_[local]__[hash:base64:5]'
+// 						}
+// 					}
+// 				]
+// 			}
+//     ]
+//   }
+// }
+
 module.exports = {
-  entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
-  mode: 'development',
+  entry: './client/index.jsx',
   output: {
-    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
-    filename: './public/bundle.js'
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
   },
-  devtool: 'source-maps',
   module: {
-    rules  : [
+    rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
-      },
-      {
-				test: /\.css$/,
-				use: [{
-					loader: 'style-loader'
-					}, {
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: '[name]_[local]__[hash:base64:5]'
-						}
-					}
-				]
-			}
+      }
     ]
-  }
-}
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+};
